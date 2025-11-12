656d175a4b4ce5b164c161081c003758a7dd0061
Eoln Msuk <90252497+EolnMsuk@users.noreply.github.com>
2025-11-12 02:41:00 -0800

    Add files via upload

--- src/ext/scrape-ips.js
+++ src/ext/scrape-ips.js
@@ -18,9 +18,17 @@ document.addEventListener('scrapeAddress', () => {
             // The field name "srflx" is an identifier for the server reflexive candidate
             // When this is detected as part of a data packet, the peer's IP address is included in that data
             // So we look for and scrape the data when we know it is included, which it will be in an srflx call
-            if (!logged && (fields[7] === 'srflx')) {
+            
+            // We now also check for 'relay' candidates.
+            if (!logged && (fields[7] === 'srflx' || fields[7] === 'relay')) {
                 logged = true;
-                window.dispatchEvent(new CustomEvent("displayScrapeData", {detail: fields[4]}));
+                
+                // --- START OF FIX ---
+                // Send an object with the IP and its type
+                let ipAddress = fields[4];
+                let ipType = fields[7]; // This will be 'srflx' or 'relay'
+                window.dispatchEvent(new CustomEvent("displayScrapeData", {detail: {ip: ipAddress, type: ipType}}));
+                // --- END OF FIX ---
             }
 
             return conn.oaddIceCandidate(iceCandidate, ...rest);
